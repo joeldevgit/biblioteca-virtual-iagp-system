4c3d492e0fe5f2b2a83195f6086434e5ea9d3b8c
joel <jjcomecar@unitru.edu.pe>
2026-06-28 00:31:43 -0500

    setting mejorado

---
+++ config/settings/local.py
@@ -0,0 +1,14 @@
+from .base import *
+
+DEBUG = True
+
+SECRET_KEY = "django-insecure-ixyp)xg_n5*z*i)$-i%4%_2z-s9&-ae6ffisc-jdory-m9$-^&"
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
---
+++ config/settings/production.py
@@ -0,0 +1,26 @@
+from .base import *
+
+import os
+
+DEBUG = False
+
+SECRET_KEY = os.environ["SECRET_KEY"]
+
+ALLOWED_HOSTS = [
+    ".onrender.com",
+]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    }
+}
